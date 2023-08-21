4e4c51dfa470a6f1679ccfce0b8db9b53778d41b
Marnin-A <msmaudu@gmail.com>
2023-08-21 13:17:58 -0700

    Completed 2-strlen.c

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _strlen - Returns the length of a string
+ * @s: Array of characters
+ * Return: Length of string "s"
+ */
+
+int _strlen(char *s)
+{
+	return (sterlen(*s));
+}
