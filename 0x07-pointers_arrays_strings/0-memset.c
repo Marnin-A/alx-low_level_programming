a004b362ddb5bd4759bcece227643d5fe6375156
Marnin-A <msmaudu@gmail.com>
2023-08-28 16:05:32 -0700

    Fixed error

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -6,16 +6,20 @@
  * @b: The constant byte b
  * @n: The number of bytes pointed to
  * Returns a pointer to the memory area s
- */
+ /
 
-char *_memset(char *s, char b, unsigned int n)
+ char *_memset(char *s, char b, unsigned int n)
 {
-	unsigned int i = 0;
+	/**
+	 * declare an unsigned int
+	 * because we are storing a
+	 * value that will always be
+	 * non-negative (zero or positive)
+	*/
+	unsigned int i;
 
-	while (i < n)
-	{
+	for (i = 0; i < n; i++)
 		s[i] = b;
-		i++;
-	}
+
 	return (s);
 }
