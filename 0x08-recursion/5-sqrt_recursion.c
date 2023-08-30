18df124c02d951397d5d0f0d05e2e05b73d41bdd
Marnin-A <msmaudu@gmail.com>
2023-08-30 14:47:44 -0700

    Fixed errors in 5-sqrt_recursion.c

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,50 +1,33 @@
 #include "main.h"
 
-/**
- * _sqrt_recursive_helper - function for calculating the natural square root
- * @n: The number for which to find the square root
- * @start: The lower bound of the current search range
- * @end: The upper bound of the current search range
- *
- * Return: the resulting square root
- */
-int _sqrt_recursive_helper(int n, int start, int end)
-{
-	int mid = (start + end) / 2;
-
-	if (mid * mid == n)
-	{
-		return (mid);
-	}
-
-	if (start > end)
-	{
-		return (-1);
-	}
-
-	if (mid * mid > n)
-		return (_sqrt_recursive_helper(n, start, mid - 1));
-	return (_sqrt_recursive_helper(n, mid + 1, end));
-}
+int sqrt_recursion_helper(int n, int i);
 
 /**
  * _sqrt_recursion - returns the natural square root of a number
- * @n: The number for which to find the square root
+ * @n: number to calculate the square root of
  *
  * Return: the resulting square root
  */
-
 int _sqrt_recursion(int n)
 {
 	if (n < 0)
-	{
 		return (-1);
-	}
+	return (sqrt_recursion_helper(n, 0));
+}
 
-	if (n == 0 || n == 1)
-	{
-		return (n);
-	}
-
-	return (_sqrt_recursive_helper(n, 1, n));
+/**
+ * sqrt_recursion_helper - recurses to find the natural
+ * square root of a number
+ * @n: number to calculate the sqaure root of
+ * @i: iterator
+ *
+ * Return: the resulting square root
+ */
+int sqrt_recursion_helper(int n, int i)
+{
+	if (i * i > n)
+		return (-1);
+	if (i * i == n)
+		return (i);
+	return (sqrt_recursion_helper(n, i + 1));
 }
