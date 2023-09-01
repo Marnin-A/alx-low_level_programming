d5a0eef35ba6d295ab627e2860db65bc62aac442
Marnin-A <msmaudu@gmail.com>
2023-09-01 13:51:44 -0700

    Completed 0x09-static_libraries Task 0

---
+++ 0x09-static_libraries/2-strlen.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _strlen - Returns the length of a string
+ * @s: Array of characters
+ * Return: Length of string "s"
+ */
+
+int _strlen(char *s)
+{
+	int i;
+
+	i = 0;
+	while (*(s + i) != 0)
+	{
+		i++;
+	}
+	return (i);
+}
