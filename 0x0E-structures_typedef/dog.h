7b8b62d24f9df118d22fab0a2397cd77ef4f9745
Marnin-A <msmaudu@gmail.com>
2023-09-11 13:43:08 -0700

    Completed tasks

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,13 +1,22 @@
 #ifndef DOG_H
 #define DOG_H
 
+/**
+ * dog_t - Typedef for struct of type dog
+ */
 typedef struct dog dog_t;
 
+/**
+ * struct dog - Struct that stores some information of a dog
+ * @name: Name of dog
+ * @age: Age of dog
+ * @owner: Owner of dog
+ * Description: Struct called "dog" that stores its name, its age,
+ * and the name of its owner
+ */
 struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-}
-
-#endif
+};
