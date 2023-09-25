c588a87e4a4469991d96e39730f9877fc554b483
Marnin-A <msmaudu@gmail.com>
2023-09-25 13:23:29 -0700

    Changed loop to while loop

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -9,17 +9,18 @@
  */
 size_t print_listint(const listint_t *h)
 {
-	unsigned int i;
+	unsigned int i = 0;
 	const listint_t *temp;
 
 	if (h == NULL)
 		return (0);
 
 	temp = h;
-	for (i = 0; temp; i++)
+	while (temp)
 	{
 		printf("%d \n", temp->n);
 		temp = temp->next;
+		i++;
 	}
 	return (i);
 }
