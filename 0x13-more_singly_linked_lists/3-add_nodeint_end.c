bec9a0900fad496e7df214e9ec5f725c827463ec
Marnin-A <msmaudu@gmail.com>
2023-09-26 11:35:58 -0700

    Changed error return values to NULL, and first node retun

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -14,18 +14,18 @@ listint_t *add_nodeint_end(listint_t **head, const int n)
 	listint_t *new, *temp;
 
 	if (head == NULL)
-		return (0);
+		return (NULL);
 	new = malloc(sizeof(listint_t));
 
 	if (new == NULL)
-		return (0);
+		return (NULL);
 	new->n = n;
 	new->next = NULL;
 	temp = *head;
 	if (*head == NULL)
 	{
 		*head = new;
-		return (0);
+		return (new);
 	}
 
 	while (temp->next != NULL)
