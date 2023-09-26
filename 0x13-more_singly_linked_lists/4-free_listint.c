5326a4b9e869d72d5382a8945187a3d8f3ea6e6f
Marnin-A <msmaudu@gmail.com>
2023-09-26 12:59:20 -0700

    Added check for null header

--- 0x13-more_singly_linked_lists/4-free_listint.c
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -14,10 +14,14 @@ void free_listint(listint_t *head)
 {
 	listint_t *next;
 
+	if (head == NULL)
+		return (NULL);
+
 	while (head != NULL)
 	{
 		next = head->next;
 		free(head);
 		head = next;
 	}
+	return (0);
 }
